c017c14632c83afd714dec75e7ccedbacda86cd1
kfcccpro-ship-it <kfcccpro@gmail.com>
2026-04-27 20:57:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -285,6 +285,9 @@ io.on('connection',(socket)=>{
 });
 
 app.use(express.static(path.join(__dirname,'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 server.listen(PORT,'0.0.0.0',()=>{
   const nets=os.networkInterfaces();
   console.log('\n========================================');
